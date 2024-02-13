765d588a89e8b78b34c9b3ec9524f36d18e133be
Ashray <78349565+AshrayVB@users.noreply.github.com>
2024-02-13 19:19:29 +0530

    Update BSTImplementation.c

--- Trees/BSTImplementation.c
+++ Trees/BSTImplementation.c
@@ -1,146 +1,139 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct Node {
+struct Node{
     int data;
-    struct Node *left;
-    struct Node *right;
+    struct Node *left,*right;
 };
+typedef struct Node*NODE;
+NODE root=NULL;
 
-void IO(struct Node *root) { // Inorder traversing: left->root->right
-    if (root == NULL) {
-        return;
-    } else {
-        IO(root->left);
-        printf("%d ", root->data);
-        IO(root->right);
+void insert(int ele){
+    NODE nn=(NODE)malloc(sizeof(struct Node));
+    nn->data=ele;
+    nn->left=nn->right=NULL;
+    if(root==NULL){
+        root=nn;
+        return ;
     }
-}
-
-void PREO(struct Node *root) { // Preorder traversing: root->left->right
-    if (root == NULL) {
-        return;
-    } else {
-        printf("%d ", root->data);
-        PREO(root->left);
-        PREO(root->right);
-    }
-}
-
-void POST(struct Node *root) { // Postorder traversing: left->right->root
-    if (root == NULL) {
-        return;
-    } else {
-        POST(root->left);
-        POST(root->right);
-        printf("%d ", root->data);
-    }
-}
-
-struct Node *getnode() { // getnode: Allocates memory for a new node and returns a pointer to it
-    struct Node *x;
-    x = (struct Node *)malloc(sizeof(struct Node));
-    if (x == NULL) {
-        printf("Out of Memory\n");
-        exit(0);
-    }
-    return x;
-}
-
-struct Node *ins(int item, struct Node *root) { // ins: Inserts a new node with the specified data into the BST while maintaining the BST property
-    struct Node *temp, *curr, *prev;
-    temp = getnode();
-    temp->data = item;
-    temp->left = NULL;
-    temp->right = NULL;
-    if (root == NULL) {
-        return temp;
-    }
-    prev = NULL;
-    curr = root;
-    while (curr != NULL) {
-        prev = curr;
-        if (item == curr->data) {
-            printf("Duplicate not allowed\n");
-            free(temp);
-            return root;
-        }
-        if (item < curr->data)
-            curr = curr->left;
+    NODE curr = root, prev=NULL;
+    while(curr!=NULL){
+        prev=curr;
+        if(ele<curr->data)
+            curr=curr->left;
         else
-            curr = curr->right;
+            curr=curr->right;
     }
-    if (item < prev->data)
-        prev->left = temp;
+    if(ele<prev->data)
+        prev->left=nn;
     else
-        prev->right = temp;
-    return root;
+        prev->right=nn;
 }
 
-int search(int item, struct Node *root) {
-    struct Node *curr = root;
-    while (curr != NULL) {
-        if (curr->data == item) {
-            return 1; // Element found
+void search(int ele){
+    if(root == NULL){
+        printf("Tree is empty\n");
+        return;
+    }
+    NODE curr = root;
+    while(curr != NULL){
+        if(ele == curr->data){
+            printf("Search successful\n");
+            return;
         }
-        if (item < curr->data)
+        else if(ele < curr->data)
             curr = curr->left;
         else
             curr = curr->right;
     }
-    return 0; // Element not found
+    printf("Search not found\n");
 }
 
-void main() {
-    struct Node *root = NULL;
-    int ch, item;
-    while (1) {
-        printf("\n1)Insert 2)Preorder Traversal 3)Postorder Traversal \n4)Inorder Traversal 5)Search 6)Exit\nEnter choice :");
-        scanf("%d", &ch);
-        switch (ch) {
-            case 1:
-                printf("Enter item :");
-                scanf("%d", &item);
-                root = ins(item, root);
-                break;
-            case 2:
-                if (root == NULL) {
-                    printf("Tree is Empty\n");
-                    exit(0);
-                }
-                printf("Preorder Traversal\n");
-                PREO(root);
-                break;
-            case 3:
-                if (root == NULL) {
-                    printf("Tree is Empty\n");
-                    exit(0);
-                }
-                printf("Postorder Traversal\n");
-                POST(root);
-                break;
-            case 4:
-                if (root == NULL) {
-                    printf("Tree is Empty\n");
-                    exit(0);
-                }
-                printf("Inorder Traversal\n");
-                IO(root);
-                break;
-            case 5:
-                printf("Enter item to search : ");
-                scanf("%d", &item);
-                if (search(item, root)) {
-                    printf("Element found\n");
-                } else {
-                    printf("Element not found\n");
-                }
-                break;
-            case 6:
-                printf("Program Closed\n");
-                exit(0);
-            default:
-                printf("Invalid Choice\n");
+void INO(NODE root){
+    if(root==NULL){
+        return;
+    }
+    INO(root->left);
+    printf("%d ",root->data);
+    INO(root->right);
+}
+
+void PREE(NODE root){
+    if(root==NULL){
+        return;
+    }
+    printf("%d ",root->data);
+    PREE(root->left);
+    PREE(root->right);
+}
+
+void POST(NODE root){
+    if(root==NULL){
+        return;
+    }
+    POST(root->left);
+    POST(root->right);
+    printf("%d ",root->data);
+}
+
+void Min(NODE root){
+    if(root==NULL){
+        printf("Trees is empty");
+        return;
+    }
+    NODE curr=root;
+    while(curr->left!=NULL){
+        curr=curr->left;
+    }
+    printf("The minimum element is %d",curr->data);
+}
+
+void Max(NODE root){
+    if(root==NULL){
+        printf("Tree is empty");
+        return;
+    }
+    NODE curr=root;
+    while(curr->right!=NULL){
+        curr=curr->right;
+    }
+    printf("The maximum element is %d",curr->data);
+}
+
+int main(){
+    int ch,ele,n;
+    while(1){
+        printf("\nEnter the choice :\n1.insert,2.search,3.Inorde,4.Postorder,\n5.Preeorder,6.Minimum,7.Maximum,8.exit\n");
+        scanf("%d",&ch);
+        switch(ch){
+            case 1: printf("Enter the NU of Element :");
+                    scanf("%d",&n);
+                    printf("Enter the element :");
+                    for(int i=0;i<n;i++){
+                        scanf("%d",&ele);
+                        insert(ele);
+                    }
+            break;
+            case 2 :printf("enter the element :");
+                    scanf("%d",&ele);
+                    search(ele);
+            break;
+            case 3:printf("Inorder : ");
+                    INO(root);
+            break;
+            case 4 :printf("Preeorder :");
+                    PREE(root);
+            break;
+            case 5 : printf("Postorder :");
+                    POST(root);
+            break;
+            case 6 : Min(root);
+            break;
+            case 7 : Max(root);
+            break;
+            case 8 : exit(0);
+            default:printf("Enter the valid choise : \n");
+                    
         }
     }
 }
